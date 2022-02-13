dfaf2bd94da323bb847f3b7b491e4eccc3c92916
MishynMykhailo <misham18092001@gmail.com>
2022-02-13 13:12:40 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMarkup = ({preview,original,description}) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`
+}
+
+const galleryMarkupItem = galleryItems.map(galleryMarkup).join("");
+
+const galleryEl = document.querySelector("ul.gallery")
+
+galleryEl.insertAdjacentHTML('beforeend',galleryMarkupItem)
+
+let gallery = new SimpleLightbox('.gallery a', {captions:true,captionsData:'alt',captionDelay:250,});
